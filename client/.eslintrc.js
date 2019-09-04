1110cfa90696dc095a59ec8d29fcac06b3a85ec7
Carsten Pleiser <carsten@eventtechpartner.com>
2019-09-04 19:04:57 +0100

    Prepared client directory

---
+++ client/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: [
+    // add more generic rulesets here, such as:
+    // 'eslint:recommended',
+    'plugin:vue/recommended'
+  ],
+  rules: {
+    'no-console': 'off',
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+  }
+}
