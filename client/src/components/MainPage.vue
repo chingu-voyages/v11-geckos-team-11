77977e3ebd1c744ea909ad1c9e1cceb0ca22a2f0
Deniz <denizderezzed@gmail.com>
2019-09-04 21:43:05 +0200

    MainPage: created and setup for child components

---
+++ client/src/components/MainPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container-fluid">
+    <!-- HEADER -->
+    <!-- HERO WITH CONTENT -->
+    <!-- WHY JOIN SECTION -->
+  </div>
+</template>
+
+<script>
+// DAS MUSS ERSTELLT UND IMPORTIERT WERDEN, ICH HABS NUR SCHON MAL HINGESCHRIEBEN
+// import Header from './MainPage/Header.vue';
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+  .container-fluid {
+    background-color: grey;
+    height: 100vh;
+    width: 100vw;
+  }
+</style>
