ea9992aa73dff2b96de2beeba5f7b20ee7bedc26
Carsten Pleiser <carsten@eventtechpartner.com>
2019-09-04 19:01:17 +0100

    Set up directory structure for server

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Whats Up Gents');
+})
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+})
