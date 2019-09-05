f8c4d2535f2d8ef9d9fc4278a04441efeea44a0b
Pablo <pabloalbendea3@gmail.com>
2019-09-05 16:22:26 +0200

    Basic Register Form content

--- client/src/components/RegisterPage/RegisterForm.vue
+++ client/src/components/RegisterPage/RegisterForm.vue
@@ -1,5 +1,26 @@
 <template>
-
+  <div class="register-form">
+    <div class="text-center">
+      <h3 class="main-header">Great to have you.</h3>
+      <h3 class="sub-header">Register. It's free.</h3>
+      <form action="">
+        <div class="form-group">
+          <input type="text" placeholder="Email">
+        </div>
+        <div class="form-group">
+          <input type="text" placeholder="Password">
+        </div>
+        <div class="form-group">
+          <input type="text" placeholder="Password Confirm">
+        </div>
+        <button type="submit">Join Now</button>
+      </form>
+      <div>
+        Already registered?
+        <a href="#">Log In Here</a>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,4 +29,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
 </style>
