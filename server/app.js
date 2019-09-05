731d96b35ccb8435231defa6949e43e25509f5b9
Carsten Pleiser <carsten@eventtechpartner.com>
2019-09-05 12:51:36 +0100

    Added simple Get All Users Route

--- server/app.js
+++ server/app.js
@@ -28,15 +28,11 @@ app.use(cookieParser());
 // Test Middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  console.log(req.cookies);
-  res.status(200).json({
-    status: "success",
-    message: "Hello from DevTribe backend"
-  });
+  // console.log(req.cookies);
   next();
 });
 
 // 2) ROUTES
-app.use("api/v1/users", userRouter);
+app.use("/api/v1/users", userRouter);
 
 module.exports = app;
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,7 +2,7 @@ const User = require("./../models/userModel");
 
 exports.getAllUsers = async (req, res, next) => {
   try {
-    const users = await User.find();
+    const users = await User.find({});
     res.status(200).json({
       status: "success",
       results: users.length,
