94983eb3318c38b3de4422c51157380baa41512e
Pablo <pabloalbendea3@gmail.com>
2019-09-05 19:05:43 +0200

    Register page basic style done

--- client/src/components/RegisterPage/RegisterForm.vue
+++ client/src/components/RegisterPage/RegisterForm.vue
@@ -1,21 +1,27 @@
 <template>
   <div class="register-form">
+    <div class="back-link">
+      <a href="#">Go Back</a>
+    </div>
     <div class="text-center">
       <h3 class="main-header">Great to have you.</h3>
       <h3 class="sub-header">Register. It's free.</h3>
-      <form action="">
-        <div class="form-group">
-          <input type="text" placeholder="Email">
+      <form action="POST">
+        <div class="input-group">
+          <span class="input-group-addon"><i class="glyphicon glyphicon-user">icon</i></span>
+          <input id="email" type="text" class="form-control" placeholder="Email">
         </div>
-        <div class="form-group">
-          <input type="text" placeholder="Password">
+        <div class="input-group">
+          <span class="input-group-addon"><i class="glyphicon glyphicon-eye-close">icon</i></span>
+          <input type="text" class="form-control" placeholder="Password">
         </div>
-        <div class="form-group">
-          <input type="text" placeholder="Password Confirm">
+        <div class="input-group">
+          <span class="input-group-addon"><i class="glyphicon glyphicon-eye-close">icon</i></span>
+          <input type="text" class="form-control" placeholder="Password Confirm">
         </div>
-        <button type="submit">Join Now</button>
+        <button id="submit" type="submit" class="btn btn-outline-success">Join Now</button>
       </form>
-      <div>
+      <div class="login-text">
         Already registered?
         <a href="#">Log In Here</a>
       </div>
@@ -29,5 +35,36 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.register-form {
+  background-color: white;
+  .back-link{
+    margin: 25px;
+  }
+  .text-center{
+      margin-top: 5%;
+    .main-header {
+      font-weight: 600;
+    }
+    .sub-header{
+      margin: 1.5em;
+      font-style: italic;
+    }
+    form {
+      margin: auto;
+      width: 45%;
+      .input-group {
+        .form-control {
+          margin-bottom: 30px!important;
+        }
+      }
+      button {
+        padding: 6px 40px;
+        margin: 25px 0;;
+      }
+    }
+    .login-text {
+      margin-bottom: 20px;
+    }
+  }
+}
 </style>
