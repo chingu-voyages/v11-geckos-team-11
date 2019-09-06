4e14b356f128c588bbb5eb83cf1b85d5adeda3b3
Deniz <denizderezzed@gmail.com>
2019-09-06 23:10:41 +0200

    ProfilePage: created Hero, UserName components

--- client/src/components/ProfilePage.vue
+++ client/src/components/ProfilePage.vue
@@ -1,37 +1,24 @@
 <template>
   <div>
-    <div class="profile-image">
-      <div class="hero container-fluid img-responsive">
-        <img
-          src="https://via.placeholder.com/300x300"
-          alt="..."
-          class="img-thumbnail profile position-relative"
-        >
-      </div>
-    <!-- <img
-        src="https://via.placeholder.com/1920x600"
-        class="img-fluid hero"
-        alt="Responsive image"
-      > -->
-    </div>
+    <Navigation />
+    <Hero />
+    <UserName />
   </div>
 </template>
 
 <script>
-export default {}
+import Navigation from './Navigation.vue';
+import Hero from './ProfilePage/Hero.vue';
+import UserName from './ProfilePage/UserName.vue';
+export default {
+  components: {
+    Navigation,
+    Hero,
+    UserName,
+  }
+}
 </script>
 
 <style lang="scss" scoped>
-.profile-image {
-    .hero {
-        background: url("https://via.placeholder.com/1920x600");
-        height: 600px;
-        .profile {
-          transform: translate(-50%, -50%);
-            left: 50%;
-            top: 100%;
 
-        }
-    }
-}
 </style>
---
+++ client/src/components/ProfilePage/Hero.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="hero-image">
+    <img
+      src="https://via.placeholder.com/1920x600"
+      class="img-fluid"
+      alt="Responsive image"
+    >
+    <img
+      src="https://via.placeholder.com/300x300"
+      alt="..."
+      class="img-thumbnail profile position-relative"
+    >
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.hero-image {
+    margin-bottom: -80px;
+    background-color: #ffffff;
+    .profile {
+        transform: translate(-50%, -50%);
+        left: 50%;
+        top: 100%;
+
+    }
+}
+</style>
