98330460f9c0c18107764714f28696e851402d4b
Carsten Pleiser <carsten@eventtechpartner.com>
2019-09-06 11:46:37 +0100

    Add catchAsync helper to avoid ugly Try/Catch syntax

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,21 +1,14 @@
 const User = require("./../models/userModel");
+const catchAsync = require("./../utils/catchAsync");
 
-exports.getAllUsers = async (req, res, next) => {
-  try {
-    const users = await User.find({});
-    res.status(200).json({
-      status: "success",
-      results: users.length,
-      data: { users }
-    });
-    // Send Response
-  } catch (err) {
-    res.status(404).json({
-      status: "error",
-      data: { err }
-    });
-  }
-};
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find({});
+  res.status(200).json({
+    status: "success",
+    results: users.length,
+    data: { users }
+  });
+});
 
 exports.createUser = (req, res) => {
   res.status(500).json({
