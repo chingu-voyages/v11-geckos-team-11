352347a06a5a3aebdf21d590a2bc5903ec57a3db
Deniz <denizderezzed@gmail.com>
2019-09-06 18:40:09 +0200

    WIP: about to align the profile image for mobile

---
+++ client/src/components/ProfilePage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="profile-image">
+      <img
+        src="https://via.placeholder.com/1920x600"
+        class="img-fluid hero"
+        alt="Responsive image"
+      >
+      <img
+        src="https://via.placeholder.com/450x450"
+        alt="..."
+        class="img-thumbnail profile position-absolute"
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.profile-image {
+    .hero {}
+    .profile {
+        top: 40%;
+        left: 50%;
+        transform: translate(-50%, 0);
+
+    }
+}
+</style>
