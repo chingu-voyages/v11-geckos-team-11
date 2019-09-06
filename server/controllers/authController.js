1759b005b3d5cdd0d9b5377fe8914ff783268975
Carsten Pleiser <carsten@eventtechpartner.com>
2019-09-06 19:20:57 +0100

    Fix problem in login func

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -52,9 +52,8 @@ exports.login = catchAsync(async (req, res, next) => {
   // 2) Check if user exists & password correct
   // Adding password back to output
   const user = await User.findOne({ email }).select("+password");
-  const correct = await user.correctPassword(password, user.password);
 
-  if (!user || !correct) {
+  if (!user || !(await user.correctPassword(password, user.password))) {
     return next(new AppError("Incorrect email or password", 401));
   }
 
