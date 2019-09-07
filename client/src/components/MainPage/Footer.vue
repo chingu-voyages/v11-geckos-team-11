c483f137bf5394403767089116e7062edea1b2f6
Pablo <pabloalbendea3@gmail.com>
2019-09-07 18:25:33 +0200

    Added basic styling and random content/links

--- client/src/components/MainPage/Footer.vue
+++ client/src/components/MainPage/Footer.vue
@@ -1,17 +1,71 @@
 <template>
-    <div class="footer pt-4 pb-4">
-        <div class="text-center">
-            This is the footer
+  <div class="footer">
+    <footer class="py-4 px-4">
+        <div class="row mx-0">
+            <div class= "col-md-4">
+                <h5>© DevTribe</h5>
+                <p class="font-italic">Lorem ipsum chambray occupy shoreditch lumbersexual. Four loko gluten-free taxidermy tofu kickstarter direct 
+                    trade knausgaard vegan.</p>
+            </div>
+            <div class="col-md-4">
+                <h4>Useful links</h4>
+                <ul class="list-unstyled">
+                    <li>
+                        <a href="#">Cool stuff</a>
+                    </li>
+                    <li>
+                        <a href="#">Random thing</a>
+                    </li>
+                    <li>
+                        <a href="#">Yes, another one</a>
+                    </li>
+                    <li>
+                        <a href="#">Okay, I'll stop now</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+            <h4>About</h4>
+                <ul class="list-unstyled">
+                    <li>
+                        <a href="#">Resource</a>
+                    </li>
+                    <li>
+                        <a href="#">Another one</a>
+                    </li>
+                    <li>
+                        <a href="#">Random link</a>
+                    </li>
+                    <li>
+                        <a href="#">Terms and conditions</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
         </div>
-    </div>
+        <div class="creators font-italic text-muted text-center">
+            Designed with love by some Chingu crazy web developers.
+        </div>
+    </footer>
+  </div>
 </template>
 
 <script>
-export default {}
+    export default {};
 </script>
 
 <style lang="scss" scoped>
     .footer {
         background-color: #424242;
-    }
+        color: #ffffff;
+        a{
+            color: #ffffff;
+            font-size: smaller;
+        }
+        .creators {
+            font-size: smaller;
+        }
+}
 </style>
