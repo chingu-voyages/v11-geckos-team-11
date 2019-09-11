96a0b9fd0443a2e870a5046553e8e527fbe5f076
Pablo <pabloalbendea3@gmail.com>
2019-09-11 17:30:09 +0200

    Created basic ResetPage.vue functioning file

---
+++ client/src/components/ResetPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="reset-page">
+      <Navigation />
+      <h1 class="my-5">This is the reset password webpage</h1>
+      <Footer />
+  </div>
+</template>
+
+<script>
+import Navigation from './Navigation.vue';
+import Footer from './MainPage/Footer.vue';
+export default {
+  components: {
+    Navigation,
+    Footer
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.reset-page {
+    background-color: #ffffff;
+}
+</style>
