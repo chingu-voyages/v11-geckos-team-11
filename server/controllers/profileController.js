5a90c091418fc8d3c4730f639fc1255ed3c50228
Carsten Pleiser <carsten@eventtechpartner.com>
2019-09-15 11:39:43 +0100

    Add profile controller and add create/update route

---
+++ server/controllers/profileController.js
@@ -0,0 +1,47 @@
+const Profile = require("./../models/profileModel");
+const catchAsync = require("./../utils/catchAsync");
+
+exports.create = catchAsync(async (req, res) => {
+  const {
+    company,
+    website,
+    location,
+    bio,
+    status,
+    githubusername,
+    skills,
+    youtube,
+    facebook,
+    twitter,
+    instagram,
+    linkedin
+  } = req.body;
+
+  // Build profile object
+  const profileFields = {};
+  profileFields.user = req.user.id;
+  if (company) profileFields.company = company;
+  if (website) profileFields.website = website;
+  if (location) profileFields.location = location;
+  if (bio) profileFields.bio = bio;
+  if (status) profileFields.status = status;
+  if (githubusername) profileFields.githubusername = githubusername;
+  if (skills) {
+    profileFields.skills = skills.split(",").map(skill => skill.trim());
+  }
+
+  // Build social object
+  profileFields.social = {};
+  if (youtube) profileFields.social.youtube = youtube;
+  if (twitter) profileFields.social.twitter = twitter;
+  if (facebook) profileFields.social.facebook = facebook;
+  if (linkedin) profileFields.social.linkedin = linkedin;
+  if (instagram) profileFields.social.instagram = instagram;
+
+  const profile = await Profile.findOneAndUpdate(
+    { user: req.user.id },
+    { $set: profileFields },
+    { new: true, upsert: true }
+  );
+  res.json(profile);
+});
--- server/routes/profileRoutes.js
+++ server/routes/profileRoutes.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const authController = require("./../controllers/authController");
+const profileController = require("./../controllers/profileController");
 
 const router = express.Router();
+
+router.post("/", authController.protect, profileController.create);
 
 module.exports = router;
