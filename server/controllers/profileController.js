4d7f59149c51caa2d620025a282b375918521d8e
Carsten Pleiser <carsten@eventtechpartner.com>
2019-09-15 16:29:48 +0100

    Add Education routes and GitHub repos

--- server/controllers/profileController.js
+++ server/controllers/profileController.js
@@ -1,13 +1,20 @@
+const request = require("request");
 const Profile = require("./../models/profileModel");
 const User = require("./../models/userModel");
 const AppError = require("./../utils/appError");
 const catchAsync = require("./../utils/catchAsync");
 
+// @route    GET api/v1/profile
+// @desc     Get all profiles
+// @access   Public
 exports.getAllProfiles = catchAsync(async (req, res) => {
   const profiles = await Profile.find().populate("user", ["email"]);
   res.json(profiles);
 });
 
+// @route    POST api/v1/profile
+// @desc     Create or update user profile
+// @access   Private
 exports.createProfile = catchAsync(async (req, res) => {
   const {
     company,
@@ -53,6 +60,9 @@ exports.createProfile = catchAsync(async (req, res) => {
   res.json(profile);
 });
 
+// @route    GET api/v1/profile/user/:user_id
+// @desc     Get profile by user ID
+// @access   Public
 exports.getProfile = catchAsync(async (req, res, next) => {
   const profile = await Profile.findOne({
     user: req.params.user_id
@@ -63,6 +73,9 @@ exports.getProfile = catchAsync(async (req, res, next) => {
   res.json(profile);
 });
 
+// @route    DELETE api/v1/profile
+// @desc     Delete profile, user & posts
+// @access   Private
 exports.deleteProfile = catchAsync(async (req, res) => {
   // Remove Profile
   await Profile.findOneAndRemove({ user: req.user.id });
@@ -70,6 +83,9 @@ exports.deleteProfile = catchAsync(async (req, res) => {
   await User.findOneAndRemove({ _id: req.user.id });
 });
 
+// @route    PUT api/v1/profile/experience
+// @desc     Add profile experience
+// @access   Private
 exports.addExperience = catchAsync(async (req, res) => {
   const { title, company, location, from, to, current, description } = req.body;
 
@@ -91,6 +107,9 @@ exports.addExperience = catchAsync(async (req, res) => {
   res.json(profile);
 });
 
+// @route    DELETE api/v1/profile/experience/:exp_id
+// @desc     Delete experience from profile
+// @access   Private
 exports.deleteExperience = catchAsync(async (req, res, next) => {
   const foundProfile = await Profile.findOne({ user: req.user.id });
   const expIds = foundProfile.experience.map(exp => exp._id.toString());
@@ -102,4 +121,74 @@ exports.deleteExperience = catchAsync(async (req, res, next) => {
   foundProfile.experience.splice(removeIndex, 1);
   await foundProfile.save();
   return res.status(200).json(foundProfile);
+});
+
+// @route    PUT api/v1/profile/education
+// @desc     Add profile education
+// @access   Private
+exports.addEducation = catchAsync(async (req, res) => {
+  const {
+    school,
+    degree,
+    fieldofstudy,
+    from,
+    to,
+    current,
+    description
+  } = req.body;
+
+  const newEdu = {
+    school,
+    degree,
+    fieldofstudy,
+    from,
+    to,
+    current,
+    description
+  };
+
+  const profile = await Profile.findOne({ user: req.user.id });
+
+  profile.education.unshift(newEdu);
+
+  await profile.save();
+  res.json(profile);
+});
+
+// @route    DELETE api/v1/profile/education/:edu_id
+// @desc     Delete education from profile
+// @access   Private
+exports.deleteEducation = catchAsync(async (req, res, next) => {
+  const foundProfile = await Profile.findOne({ user: req.user.id });
+  const eduIds = foundProfile.education.map(edu => edu._id.toString());
+  const removeIndex = eduIds.indexOf(req.params.edu_id);
+
+  if (removeIndex === -1) {
+    return next(new AppError("Cannot delete education", 400));
+  }
+  foundProfile.education.splice(removeIndex, 1);
+  await foundProfile.save();
+  return res.status(200).json(foundProfile);
+});
+
+// @route    GET api/v1/profile/github/:username
+// @desc     Get user repos from Github
+// @access   Public
+exports.getGithub = catchAsync(async (req, res, next) => {
+  const options = {
+    uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${process.env.GITHUB_CLIENT_ID}&client_secret=${process.env.GITHUB_CLIENT_SECRET}`,
+    method: "GET",
+    headers: { "user-agent": "node.js" }
+  };
+
+  console.log(options);
+  request(options, (error, response, body) => {
+    if (error) console.error(error);
+
+    if (response.statusCode !== 200) {
+      return next(new AppError("No Github profile found", 404));
+    }
+
+    res.json(JSON.parse(body));
+  });
 });
--- server/routes/profileRoutes.js
+++ server/routes/profileRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.get("/", profileController.getAllProfiles);
 router.get("/user/:user_id", profileController.getProfile);
+router.get("/github/:username", profileController.getGithub);
 
 // Protected routes
 router.use(authController.protect);
@@ -13,5 +14,7 @@ router.post("/", profileController.createProfile);
 router.delete("/", profileController.deleteProfile);
 router.put("/experience", profileController.addExperience);
 router.delete("/experience/:exp_id", profileController.deleteExperience);
+router.put("/education", profileController.addEducation);
+router.delete("/education/:edu_id", profileController.deleteEducation);
 
 module.exports = router;
