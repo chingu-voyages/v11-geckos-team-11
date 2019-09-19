777ff7b41dd2a5440d75fd46bb7a387d69300e2b
Deniz <denizderezzed@gmail.com>
2019-09-19 15:41:00 +0200

    WIP: styling posts-box

--- client/src/components/PostPage/Posts.vue
+++ client/src/components/PostPage/Posts.vue
@@ -5,18 +5,22 @@
   >
     <div class="posts-box border border-dark">
       <div class="row">
-        <div class="col">
+        <div class="col-4 user mx-auto my-auto">
           <img
-            src="https://via.placeholder.com/100x100"
-            class="img-fluid"
+            src="https://via.placeholder.com/120x100"
+            class="img-fluid text-center"
             alt="Responsive image"
           >
-          <div class="user">
-            <p>John Doe</p>
-            <p>Member since May 2018</p>
+          <div>
+            <div class="username text-center">
+              John Doe
+            </div>
+            <div class="member-date">
+              Member since May 2018
+            </div>
           </div>
         </div>
-        <div class="col">
+        <div class="col-7 mx-auto my-auto">
           <div class="text-box">
             <div class="text">
               Make your work consistent but not fucking
@@ -25,12 +29,20 @@
               it.” When you design, you have to draw on your own fucking life
               experiences. If it’s not something you would
             </div>
-            <div class="date">posted on 17 July 2019</div>
+            <div class="posted-date">
+              posted on 17 July 2019
+            </div>
           </div>
           <div class="votings">
             <span class="up-vote badge badge-success">12</span>
             <span class="down-vote badge badge-danger">1</span>
-            <button type="button" class="btn btn-outline-primary">Start Discussion</button>
+            <button
+              type="button"
+              class="btn btn-outline-primary"
+            >
+              Start
+              Discussion
+            </button>
           </div>
         </div>
       </div>
@@ -51,5 +63,13 @@ export default {
 
 <style lang="scss" scoped>
 #posts {
-    }
+  height: 100%;
+
+
+  .posts-box {
+    height: 200px;
+    font-size: 14px;
+  }
+
+}
 </style>
