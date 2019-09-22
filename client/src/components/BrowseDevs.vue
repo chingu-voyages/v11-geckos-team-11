5d0a3693094787c4fea97814966b48d50a34589b
Deniz <denizderezzed@gmail.com>
2019-09-22 16:12:27 +0200

    refactoring

--- client/src/components/BrowseDevs.vue
+++ client/src/components/BrowseDevs.vue
@@ -5,48 +5,19 @@
         Browse & Connect With Developers
       </div>
     </div>
-
-    <div class="dev-card w-75 mx-auto mt-5 border border-dark">
-      <div class="row px-3 py-4">
-        <div class="col-md-3 user mx-auto my-auto text-center">
-          <img
-            src="https://via.placeholder.com/200x150"
-            class="img-fluid text-center"
-            alt="user profile image"
-          >
-        </div>
-        <div class="col-md-6">
-          <div class="user-data mb-2">
-            <div class="user-name h3">
-              John Doe
-            </div>
-            <div class="user-role font-weight-bold h5">
-              Senior Frontend Developer
-            </div>
-            <div class="user-location">
-              <i class="pr-2 fas fa-map-marker-alt h3" />
-              Hamburg, Germany
-            </div>
-            <button class="btn btn-outline-warning mt-2">
-              View Profile
-            </button>
-          </div>
-        </div>
-        <div class="col-md-3">
-          <ul class="user-skills">
-            <li><i class="fas fa-check-circle pr-2" />JavaScript</li>
-            <li><i class="fas fa-check-circle pr-2" />HTML</li>
-            <li><i class="fas fa-check-circle pr-2" />CSS</li>
-          </ul>
-        </div>
-      </div>
-    </div>
+    <DevCard />
+    <DevCard />
+    <DevCard />
   </div>
 </template>
 
 <script>
+import DevCard from "./BrowseDevs/DevCard.vue";
 export default {
   name: "BrowseDevs",
+  components: {
+    DevCard
+  },
   data() {
     return {};
   }
@@ -58,16 +29,5 @@ export default {
   height: 100%;
   background-color: #ffffff;
 
-  .dev-card {
-    .user-skills {
-      list-style-type: none;
-      li {
-        margin: 10% 0;
-        i {
-          font-size: 25px;
-        }
-      }
-    }
-  }
 }
 </style>
