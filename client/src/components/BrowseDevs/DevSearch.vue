5f8fdb91233316a633c2462e23bdc3a3f8b162f9
Deniz <denizderezzed@gmail.com>
2019-09-22 16:28:28 +0200

    added input search for BrowseDevs

--- client/src/components/BrowseDevs/DevSearch.vue
+++ client/src/components/BrowseDevs/DevSearch.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>test</div>
+  <div class="input-group mb-3 w-50 mt-5 mx-auto">
+    <input
+      type="text"
+      class="form-control"
+      placeholder="Search for Devs..."
+    >
+    <div class="input-group-append">
+      <button
+        id="button-addon2"
+        class="btn btn-outline-primary"
+        type="button"
+      >
+        Search
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
