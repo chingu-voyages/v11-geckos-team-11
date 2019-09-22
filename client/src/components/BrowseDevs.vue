f53e7949d51e2fba1055d779e946aa98760362ba
Deniz <denizderezzed@gmail.com>
2019-09-22 16:00:00 +0200

    created browse-devs page

--- client/src/components/BrowseDevs.vue
+++ client/src/components/BrowseDevs.vue
@@ -5,45 +5,39 @@
         Browse & Connect With Developers
       </div>
     </div>
-    <div class="dev-cards w-75 mx-auto mt-5 border border-dark">
+
+    <div class="dev-card w-75 mx-auto mt-5 border border-dark">
       <div class="row px-3 py-4">
         <div class="col-md-3 user mx-auto my-auto text-center">
           <img
-            src="https://via.placeholder.com/100x100"
-            class="img-fluid text-center rounded-circle"
-            alt="Responsive image"
+            src="https://via.placeholder.com/200x150"
+            class="img-fluid text-center"
+            alt="user profile image"
           >
-          <div class="mt-1">
-            <span class="username">John Doe</span>
-            <br>
-            <span class="member-date">Member since May 2018</span>
-          </div>
         </div>
-        <div class="col-md-9">
-          <div class="text-box mb-2">
-            <div class="text">
-              Make your work consistent but not fucking
-              predictable. While having drinks with Tibor Kalman one night, he
-              told me, “When you make something no one hates, no one fucking loves
-              it.” When you design, you have to draw on your own fucking life
-              experiences. If it’s not something you would.
+        <div class="col-md-6">
+          <div class="user-data mb-2">
+            <div class="user-name h3">
+              John Doe
             </div>
-            <span class="posted-date">posted on 17 July 2019</span>
-          </div>
-          <div class="votings">
-            <span class="up-vote badge badge-success mr-3"><i
-              class="fas fa-thumbs-up mx-1"
-            />12</span>
-            <span class="down-vote badge badge-danger"><i
-              class="fas fa-thumbs-down mx-1"
-            />100</span>
-            <button
-              type="button"
-              class="btn btn-outline-primary ml-3"
-            >
-              {{ $store.getters.buttonText }}
+            <div class="user-role font-weight-bold h5">
+              Senior Frontend Developer
+            </div>
+            <div class="user-location">
+              <i class="pr-2 fas fa-map-marker-alt h3" />
+              Hamburg, Germany
+            </div>
+            <button class="btn btn-outline-warning mt-2">
+              View Profile
             </button>
           </div>
+        </div>
+        <div class="col-md-3">
+          <ul class="user-skills">
+            <li><i class="fas fa-check-circle pr-2" />JavaScript</li>
+            <li><i class="fas fa-check-circle pr-2" />HTML</li>
+            <li><i class="fas fa-check-circle pr-2" />CSS</li>
+          </ul>
         </div>
       </div>
     </div>
@@ -61,8 +55,19 @@ export default {
 
 <style lang="scss" scoped>
 .browse-devs {
-	height: 100%;
-	background-color: #ffffff;
-}
+  height: 100%;
+  background-color: #ffffff;
 
+  .dev-card {
+    .user-skills {
+      list-style-type: none;
+      li {
+        margin: 10% 0;
+        i {
+          font-size: 25px;
+        }
+      }
+    }
+  }
+}
 </style>
