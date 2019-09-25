1c91a84b6ff0f732f1ebcadeefd3baea19cf740f
Deniz <denizderezzed@gmail.com>
2019-09-25 16:58:09 +0200

    trying to get data from usersModel instead creating a new schema for it

--- server/controllers/browseDevsController.js
+++
@@ -1,2 +0,0 @@
-const BrowseDevs = require('./../models/browseDevsModel');
-// https://www.codementor.io/olatundegaruba/nodejs-restful-apis-in-10-minutes-q0sgsfhbd
--- server/routes/browseDevsRoutes.js
+++
@@ -1,5 +0,0 @@
-const express = require('express');
-const browseDevsController = require('./../controllers/browseDevsController.js');
-const router = express.Router();
-
-router.get('/',browseDevsController.allUsers);
